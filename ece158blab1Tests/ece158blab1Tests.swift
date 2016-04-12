6e382edce0ff5619c3ea7b8530b83c7805692f76
Woo-jong Jang <wojang@acs-wlh2015-08.ucsd.edu>
2016-04-12 15:18:01 -0700

    Initial Commit

---
+++ ece158blab1Tests/ece158blab1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  ece158blab1Tests.swift
+//  ece158blab1Tests
+//
+//  Created by Woo-jong Jang on 4/12/16.
+//  Copyright (c) 2016 Woo-jong Jang. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class ece158blab1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
